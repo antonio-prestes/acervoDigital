d9a539c52e79e354fafd59dac03429ed10a94bb2
antonio-prestes <diegoantoniodasp@hotmail.com>
2022-09-19 18:11:34 -0300

    - Home page

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,23 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative overflow-hidden bg-white">
+    <div class="mx-auto relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-5">
+            <main class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <div class="sm:text-center lg:text-center">
+                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                        <span class="block xl:inline">Acervo</span>
+                        <span class="block text-indigo-600 xl:inline">Digital</span>
+                    </h1>
+                    <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:text-lg md:mt-5 md:text-xl lg:mx-0">
+                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                    </p>
+                    <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
+                        <div class="rounded-md shadow">
+                            <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">
+                                Crie sua conta
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+</div>
