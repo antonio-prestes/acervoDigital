86672a05f6b3f18ded78867624e0f14cac0dace4
antonio-prestes <diegoantoniodasp@hotmail.com>
2022-09-23 08:59:26 -0300

    - Contact form.

--- resources/views/components/contact-form.blade.php
+++ resources/views/components/contact-form.blade.php
@@ -17,7 +17,7 @@
                 <p class="text-gray-500 mb-2">info@gmail.com</p>
             </div>
             <div class="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">
-                <form method="POST" action="{{ route('contact.create') }}">
+                <form method="POST" action="/contact">
                     @csrf
                     <div class="form-group mb-6">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
